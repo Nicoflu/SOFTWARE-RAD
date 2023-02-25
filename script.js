c7f5e606f6076407e06188179d875e125c6e16e5
Nicoflu <113746244+Nicoflu@users.noreply.github.com>
2023-02-25 15:09:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let bg = document.querySelector("header > img")
+let root = document.querySelector("html")
+window.addEventListener('scroll', () => {
+    let { scrollY } = window;
+    bg.style.top = 0.5 * scrollY + 'px';
+    root.style.setProperty('--primary-color-hue', 230 + 0.025 * scrollY );
+    root.style.setProperty('--secondary-color-hue', 170 + 0.025 * scrollY );
+    root.style.setProperty('--hero-font-weight', 600 - 1 * scrollY );
+    root.style.setProperty('--hero-letter-spacing',  0.01 * scrollY + "px" );
+});
